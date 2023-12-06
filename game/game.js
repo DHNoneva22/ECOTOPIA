7205744fddea3ef285558c2ed82d8f67b580a2ba
SKDimitrov22 <SKDimitrov22@codingburgas.bg>
2023-12-06 14:16:29 +0200

    Start to develop the game

--- game/game.js
+++ game/game.js
@@ -1,32 +1,72 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+let score = 0;
+let difficulty = '';
+let avatar = '';
+ 
+function startGame() {
+  document.getElementById('home-screen').style.display = 'none';
+  document.getElementById('difficulty-screen').style.display = 'block';
+  document.getElementById('game-screen').style.position = 'absolute';
+  document.getElementById('difficulty-screen').style.position = 'absolute';
+  document.getElementById('difficulty-screen').style.top = '500px';
+  document.getElementById('difficulty-screen').style.left = '800px';
+}
+ 
+function setDifficulty(diff) {
+  difficulty = diff;
+  document.getElementById('difficulty-screen').style.display = 'none';
+  document.getElementById('avatar-screen').style.display = 'block';
+  document.getElementById('avatar-screen').style.position = 'absolute';
+  document.getElementById('avatar-screen').style.top = '500px';
+  document.getElementById('avatar-screen').style.left = '800px';
+}
+ 
+function setAvatar(selectedAvatar) {
+  avatar = selectedAvatar;
+  document.getElementById('avatar-screen').style.display = 'none';
+  document.getElementById('game-screen').style.display = 'block';
+  document.getElementById('game-screen').style.position = 'absolute';
+  document.getElementById('game-screen').style.top = '500px';
+  document.getElementById('game-screen').style.left = '800px';
+  startConveyorBelt();
+}
+ 
+function startConveyorBelt() {
+  const conveyorBelt = document.getElementById('conveyor-belt');
+ 
+  setInterval(() => {
+    const element = document.createElement('div');
+    element.className = 'conveyor-element';
+    const random = Math.random();
+    element.textContent = random < 0.5 ? 'Recyclable' : 'Non-Recyclable';
+   
+    conveyorBelt.innerHTML = '';
+    conveyorBelt.appendChild(element);
+  }, 2000);
+}
+ 
+function recycle() {
+  const bin = document.getElementById('bin');
+  const conveyorElement = document.querySelector('.conveyor-element');
+ 
+  if (conveyorElement) {
+    const type = conveyorElement.textContent;
+ 
+    if ((type === 'Recyclable' && bin.style.backgroundColor === '#00f') ||
+        (type === 'Non-Recyclable' && bin.style.backgroundColor !== '#00f')) {
+      score -= 1;
+    } else {
+      score += 2;
     }
-};
-
-var game = new Phaser.Game(config);
-
-
-let img = document.getElementById('img');
-      let dynamicImage = document.createElement('img');
-      dynamicImage.src = "../images/";
-      img.appendChild(dynamicImage);
-
-function preload ()
-{
-    
+ 
+    document.getElementById('score').textContent = score;
+    conveyorElement.remove();
+  }
 }
-
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-}
-
-function update ()
-{
-}
+ 
+class ConveyorElement {
+    constructor(color, positionX, positionY) {
+      this.color = color;
+      this.positionX = positionX;
+      this.positionY = positionY;
+    }
+  }
