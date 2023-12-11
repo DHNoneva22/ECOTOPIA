f7d58d440ed5b2ef74d8ed69942f447c0874fa7a
Stoyan Hristov <SPHristov22@codingburgas.bg>
2023-12-11 21:24:13 +0200

    Make reduce page responsive

--- javascript/reduce.js
+++ javascript/reduce.js
@@ -1,89 +1,140 @@
 // setup 
-const data = {
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    datasets: [{
-      label: 'Weekly Sales',
-      data: [18, 12, 6, 9, 12, 3, 9],
-      backgroundColor: [
-        'rgba(255, 26, 104, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(0, 0, 0, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 26, 104, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-        'rgba(0, 0, 0, 1)'
-      ],
-      borderWidth: 1
-    }]
-  };
+const data = [{
+  labels: ['1996', '2000', '2004', '2008', '2012', '2016', '2020'],
+  datasets: [{
+    label: 'World Polution',
+    data: [18, 12, 6, 9, 12, 3, 9],
+    backgroundColor: [
+      'rgba(255, 26, 104, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(0, 0, 0, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255, 26, 104, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+      'rgba(0, 0, 0, 1)'
+    ],
+    borderWidth: 1
+  }]
+}, {
+  labels: ['1996', '2000', '2004', '2008', '2012', '2016', '2020'],
+  datasets: [{
+    label: 'World Polution',
+    data: [18, 12, 6, 9, 12, 3, 9],
+    backgroundColor: [
+      'rgba(255, 26, 104, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(0, 0, 0, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255, 26, 104, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+      'rgba(0, 0, 0, 1)'
+    ],
+    borderWidth: 1
+  }]
+},  {
+  labels: ['1996', '2000', '2004', '2008', '2012', '2016', '2020'],
+  datasets: [{
+    label: 'World Polution',
+    data: [18, 12, 6, 9, 12, 3, 9],
+    backgroundColor: [
+      'rgba(255, 26, 104, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(0, 0, 0, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255, 26, 104, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+      'rgba(0, 0, 0, 1)'
+    ],
+    borderWidth: 1
+  }]
+}];
 
-  // config 
-  const config = {
-    type: 'bar',
-    data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+
+// config 
+const config = {
+  type: 'bar',
+  data: data[0],
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
       }
     }
-  };
+  }
+};
 
-  // render init block
-  const myChart = new Chart(
-    document.getElementById('barChart'),
-    config
-  );
+// render init block
+const myChart = new Chart(
+  document.getElementById('barChart'),
+  config
+);
 
 
-  // config 
-  const config2 = {
-    type: 'line',
-    data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+// config 
+const config2 = {
+  type: 'line',
+  data: data[1],
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
       }
     }
-  };
+  }
+};
 
-  const lineChart = new Chart(
-    document.getElementById('lineChart'),
-    config2
-  );
+const lineChart = new Chart(
+  document.getElementById('lineChart'),
+  config2
+);
 
 
-  // config 
-  const config3 = {
-    type: 'pie',
-    data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+// config 
+const config3 = {
+  type: 'pie',
+  data: data[2],
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
       }
     }
-  };
+  }
+};
 
-  const pieChart = new Chart(
-    document.getElementById('pieChart'),
-    config3
-  );
+const pieChart = new Chart(
+  document.getElementById('pieChart'),
+  config3
+);
 
-  // Instantly assign Chart.js version
-  const chartVersion = document.getElementById('chartVersion');
-  chartVersion.innerText = Chart.version;
+// Instantly assign Chart.js version
+const chartVersion = document.getElementById('chartVersion');
+chartVersion.innerText = Chart.version;
     
